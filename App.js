1eee9a56194c85deb8117f5b3e12dad37f956141
AlexGR10 <jalejandrogtzrdz@gmail.com>
2024-02-21 23:28:04 -0600

    Agregar URL imagen

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var data = [];
 
     function actualizarTabla() {
@@ -8,66 +8,113 @@ $(document).ready(function() {
         for (var i = 0; i < data.length; i++) {
             var producto = data[i];
             var fila = `<tr data-index="${i}">
-                            <td>${producto.ID}</td>
-                            <td>${producto.Categoria}</td>
-                            <td>${producto.Nombre}</td>
-                            <td>${producto.Descripcion}</td>
-                            <td>
-                            <button class="btn btn-primary btn-editar"><i class="fa-solid fa-pen-to-square"></i></button>
-                            <button class="btn btn-danger btn-eliminar"><i class="fa-solid fa-trash-can"></i></button>
-                            </td>
-                        </tr>`;
+                <td>${producto.ID}</td>
+                <td>${producto.Categoria}</td>
+                <td>${producto.Nombre}</td>
+                <td>${producto.Imagen}</td> <!-- Mostrar la URL de la imagen -->
+                <td>${producto.Descripcion}</td>
+                <td>
+                    <button class="btn btn-primary btn-editar"><i class="fa-solid fa-pen-to-square"></i></button>
+                    <button class="btn btn-danger btn-eliminar"><i class="fa-solid fa-trash-can"></i></button>
+                </td>
+            </tr>`;
+
             tabla.append(fila);
         }
     }
 
-    // Agregar o editar un producto
-    $("#agregarEditar").click(function() {
+    $("#ImagenInput").change(function () {
+        var fileInput = $(this)[0];
+
+        if (fileInput.files && fileInput.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $("#ImagenPreview").attr("src", e.target.result);
+            };
+
+            reader.readAsDataURL(fileInput.files[0]);
+        }
+    });
+
+    $("#SeleccionarImagen").click(function () {
+        $("#ImagenInput").click();
+    });
+
+    $("#agregarEditar").click(function () {
         var id = $("#ID").val();
         var categoria = $("#Categoria").val();
         var nombre = $("#Nombre").val();
         var descripcion = $("#Descripcion").val();
+
+        // Obtener la información de la imagen
+        var imagenInput = document.getElementById("ImagenInput");
+        var imagen = imagenInput.files.length > 0 ? URL.createObjectURL(imagenInput.files[0]) : '';
+
         var editIndex = $("#editIndex").val();
 
         if (editIndex === "") {
             // Agregar nuevo producto
             data.push({
-                ID: id,
+                ID: data.length + 1, // Autoincrementable
                 Categoria: categoria,
                 Nombre: nombre,
+                Imagen: imagen,
                 Descripcion: descripcion
             });
-        } else {//inicio Else
-            //Se edita un producto que ya existe
-            
+        } else {
+            // Se edita un producto que ya existe
             data[editIndex].ID = id;
-            //Data es el tipo de arreglo, el cual modificara un parametro en especifico llamando "editIndex" cuya propiedad es ID y se le asgina el valor del "id"
             data[editIndex].Categoria = categoria;
-            //El arreglo data modificara un valor en especifico llamado categoria
             data[editIndex].Nombre = nombre;
-            //El arreglo asigna el nombre del arreglo de nombre
+            data[editIndex].Imagen = imagen;
             data[editIndex].Descripcion = descripcion;
-            //El arreglo asigna la propiedad de descripción
-            $("#editIndex").val(""); //El signo "$" esta seleccionando una libreria de jQuery
-            //La propiedad "#editIndex" es un elemento especifico que coincida con el id y ".val("")" establece un valor predeterminado
-        }//fin else
+            $("#editIndex").val("");
+        }
 
-        //Limpiar formulario
-
+        // Limpiar formulario
         $("#ID").val("");
         $("#Categoria").val("");
         $("#Nombre").val("");
+        $("#Imagen").val("");
         $("#Descripcion").val("");
-        
+
         // Actualizar la tabla
         actualizarTabla();
     });
 
+    $("#lista").on("click", ".btn-editar", function () {
+        var index = $(this).closest("tr").data("index");
 
+        // Llenar el formulario con los datos de la fila seleccionada
+        var producto = data[index];
+        $("#ID").val(producto.ID);
+        $("#Categoria").val(producto.Categoria);
+        $("#Nombre").val(producto.Nombre);
+        $("#Descripcion").val(producto.Descripcion);
 
-    $("#lista").on("click", ".btn-eliminar", function() {
+        // Actualizar la vista previa de la imagen
+        var imagenInput = document.getElementById("ImagenInput");
+        var imagenURL = producto.Imagen;
+        if (imagenURL) {
+            $("#ImagenPreview").attr("src", imagenURL);
+        } else {
+            $("#ImagenPreview").attr("src", ""); // Limpiar la vista previa si no hay URL de imagen
+        }
+
+        // Guardar el índice para saber que estamos editando
+        $("#editIndex").val(index);
+    });
+
+    $("#lista").on("click", ".btn-eliminar", function () {
         var index = $(this).closest("tr").data("index");
         data.splice(index, 1);
+        // Limpiar formulario
+        $("#ID").val("");
+        $("#Categoria").val("");
+        $("#Nombre").val("");
+        $("#Imagen").val("");
+        $("#Descripcion").val("")
         actualizarTabla();
     });
 
